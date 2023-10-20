d6db0699cdc83e3ad322184b7d924479b78ab62a
Андрій <rukom@ukr.net>
2023-10-20 18:06:27 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const list = document.querySelector('.gallery');
+
+function renderList(images) {
+  const markup = images.map(({ preview, original, description }) => {
+    return `
+    <li class="gallery__item" >
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" >
+      </a>
+    </li>`
+  }).join('');
+  console.log(markup);
+  list.innerHTML = markup;
+}
+
+renderList(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+})
 
 console.log(galleryItems);
