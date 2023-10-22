4b47bbf5c0f7a509ec5551ffe81b5742d42359f0
Андрій <rukom@ukr.net>
2023-10-22 18:39:31 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('.feedback-form');
+const KEY = 'feedback-form-state';
+let dataForm = JSON.parse(localStorage.getItem(KEY)) || {};
+const { email, message } = form.elements;
+
+if (dataForm) {
+  email.value = dataForm.email || '';
+  message.value = dataForm.message || '';
+}
+
+
+form.addEventListener('input', throttle(addDataForm, 500));
+form.addEventListener('submit', formSubmit);
+
+function addDataForm() {
+  dataForm = { email: email.value, message: message.value };
+  localStorage.setItem(KEY, JSON.stringify(dataForm));
+}
+
+function formSubmit(event) {
+  event.preventDefault();
+  console.log({email: email.value, message: message.value});
+  if (email.value === '' || message.value === '') {
+    return alert('Please fill in all fields of the form!');
+  }
+
+  localStorage.removeItem(KEY);
+  event.currentTarget.reset();
+  dataForm = {};
+}
