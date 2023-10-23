758c291c9aff7634ffe22eb489d64b0baa1e85d0
Андрій <rukom@ukr.net>
2023-10-23 15:37:16 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,31 +2,36 @@ import throttle from "lodash.throttle";
 
 const form = document.querySelector('.feedback-form');
 const KEY = 'feedback-form-state';
-let dataForm = JSON.parse(localStorage.getItem(KEY)) || {};
-const { email, message } = form.elements;
 
-if (dataForm) {
-  email.value = dataForm.email || '';
-  message.value = dataForm.message || '';
-}
-
+let dataForm = {};
 
 form.addEventListener('input', throttle(addDataForm, 500));
 form.addEventListener('submit', formSubmit);
 
-function addDataForm() {
-  dataForm = { email: email.value, message: message.value };
+function addDataForm(event) {
+  dataForm[event.target.name] = event.target.value.trim();
   localStorage.setItem(KEY, JSON.stringify(dataForm));
 }
 
 function formSubmit(event) {
   event.preventDefault();
-  console.log({email: email.value, message: message.value});
-  if (email.value === '' || message.value === '') {
-    return alert('Please fill in all fields of the form!');
-  }
 
   localStorage.removeItem(KEY);
   event.currentTarget.reset();
+  console.log(dataForm);
   dataForm = {};
 }
+
+const onLoad = () => {
+  try {
+    const data = localStorage.getItem(KEY);
+    if (!data) return;
+    dataForm = JSON.parse(data);
+    Object.entries(dataForm).forEach(([key, val]) => {
+      form.elements[key].value = val;
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+onLoad();
